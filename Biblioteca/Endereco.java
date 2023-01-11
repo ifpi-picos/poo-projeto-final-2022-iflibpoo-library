39418889957a548c4352251b75d61466cc7eb860
VitorGx <118642085+VitorGx@users.noreply.github.com>
2023-01-11 01:50:26 -0300

    Update Endereco.java

---
+++ Biblioteca/Endereco.java
@@ -0,0 +1,47 @@
+public class Endereco {
+    
+    private int ceep;
+    private String nomeBairro;
+    private String nomeRua;
+    private int numCasa;
+    
+    public Endereco(int ceep, String nomeBairro, String nomeRua, int numCasa) {
+        this.ceep = ceep;
+        this.nomeBairro = nomeBairro;
+        this.nomeRua = nomeRua;
+        this.numCasa = numCasa;
+    }
+
+    public int getCeep() {
+        return ceep;
+    }
+
+    public void setCeep(int ceep) {
+        this.ceep = ceep;
+    }
+
+    public String getNomeBairro() {
+        return nomeBairro;
+    }
+
+    public void setNomeBairro(String nomeBairro) {
+        this.nomeBairro = nomeBairro;
+    }
+
+    public String getNomeRua() {
+        return nomeRua;
+    }
+
+    public void setNomeRua(String nomeRua) {
+        this.nomeRua = nomeRua;
+    }
+
+    public int getNumCasa() {
+        return numCasa;
+    }
+
+    public void setNumCasa(int numCasa) {
+        this.numCasa = numCasa;
+    }
+
+}
