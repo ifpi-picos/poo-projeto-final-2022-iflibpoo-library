f68e2aec99722aebf7d06e627dc2a4f60050b60e
VitorGx <118642085+VitorGx@users.noreply.github.com>
2023-01-11 01:42:15 -0300

    Update Livros.java

---
+++ Biblioteca/Livros.java
@@ -0,0 +1,58 @@
+public class Livros {
+    
+    private String titulo;
+    private String autor;
+    private AreaDeConhecimento areaDeConhecimento;
+    private String dataDePublicacao;
+    private int quantidadeNoAcervo;
+    
+    public Livros(String titulo, String autor, AreaDeConhecimento areaDeConhecimento, String dataDePublicacao, int quantidadeNoAcervo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.areaDeConhecimento = areaDeConhecimento;
+        this.dataDePublicacao = dataDePublicacao;
+        this.quantidadeNoAcervo = quantidadeNoAcervo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDataDePublicacao() {
+        return dataDePublicacao;
+    }
+
+    public void setDataDePublicacao(String dataDePublicacao) {
+        this.dataDePublicacao = dataDePublicacao;
+    }
+
+    public int getQuantidadeNoAcervo() {
+        return quantidadeNoAcervo;
+    }
+
+    public void setQuantidadeNoAcervo(int quantidadeNoAcervo) {
+        this.quantidadeNoAcervo = quantidadeNoAcervo;
+    }
+
+    public AreaDeConhecimento getAreaDeConhecimento() {
+        return areaDeConhecimento;
+    }
+
+    public void setAreaDeConhecimento(AreaDeConhecimento areaDeConhecimento) {
+        this.areaDeConhecimento = areaDeConhecimento;
+    }
+
+    
+}
