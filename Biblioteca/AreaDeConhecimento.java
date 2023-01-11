8353d1c340ac64bc38d7a5ae620b58ab53220970
VitorGx <118642085+VitorGx@users.noreply.github.com>
2023-01-11 01:35:16 -0300

    Update AreaDeConhecimento.java

---
+++ Biblioteca/AreaDeConhecimento.java
@@ -0,0 +1,27 @@
+public class AreaDeConhecimento {
+    
+    private String titulo;
+    private String descricao;
+    
+    public AreaDeConhecimento(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
