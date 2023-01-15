379487041e511e6e96e25f9b3e3b00cc56c6078c
PEDROHCA18 <114013463+PEDROHCA18@users.noreply.github.com>
2023-01-15 06:11:49 +0000

    Emprestimo

---
+++ .github/Biblioteca/Emprestimo.java
@@ -0,0 +1,53 @@
+package Biblioteca;
+
+public class Emprestimo {
+    
+    private String DataEmprestimo;
+    private String DataDevolucao;
+    private String Livro; 
+    private String Usuario; 
+
+    public String getDataEmprestimo() {
+        return DataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String DataEmprestimo) {
+        this.DataEmprestimo = DataEmprestimo;
+    }
+
+   
+    public String getDataDevolucao() {
+        return DataDevolucao;
+    }
+
+    
+    public void setDataDevolucao(String DataDevolucao) {
+        this.DataDevolucao = DataDevolucao;
+    }
+
+    
+    public String getLivro() {
+        return Livro;
+    }
+
+    
+    public void setLivro(String Livro) {
+        this.Livro = Livro;
+    }
+
+   
+    public String getUsuario() {
+        return Usuario;
+    }
+
+    
+    public void setUsuario(String Usuario) {
+        this.Usuario = Usuario;
+    }
+    
+    public String toString(){
+        return getLivro() + "," + getUsuario() + "," + getDataEmprestimo() +
+                "," + getDataDevolucao();
+    }
+    
+}
